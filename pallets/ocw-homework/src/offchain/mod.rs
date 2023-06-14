75364076b7006093b6b13e77f8d9e6c1ef16049a
radiancez <radiancez@126.com>
2023-06-15 00:59:15 +0800

    derived_key里的block_number只需要实现Encode

--- pallets/ocw-homework/src/offchain/mod.rs
+++ pallets/ocw-homework/src/offchain/mod.rs
@@ -1,27 +1,9 @@
-use codec::MaxEncodedLen;
-use core::fmt::Debug;
-use frame_support::Parameter;
-use scale_info::TypeInfo;
-use sp_runtime::traits::{
-	AtLeast32BitUnsigned, Bounded, MaybeDisplay, MaybeSerializeDeserialize, Member,
-};
+use codec::Encode;
 use sp_std::vec::Vec;
 
 pub(crate) fn derived_key<BN>(block_number: BN, key: &[u8]) -> Vec<u8>
 where
-	BN: Parameter
-		+ Member
-		+ MaybeSerializeDeserialize
-		+ Debug
-		+ MaybeDisplay
-		+ AtLeast32BitUnsigned
-		+ Default
-		+ Bounded
-		+ Copy
-		+ sp_std::hash::Hash
-		+ sp_std::str::FromStr
-		+ MaxEncodedLen
-		+ TypeInfo,
+	BN: Encode,
 {
 	block_number.using_encoded(|encoded_bn| {
 		key.iter().chain(b"@".iter()).chain(encoded_bn).copied().collect::<Vec<u8>>()
